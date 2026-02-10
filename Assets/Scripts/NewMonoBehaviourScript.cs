577f5e2feda4b6ddf24cc9f120d52d20ef495406
Royal-Nobody <74615786+Royal-Nobody@users.noreply.github.com>
2026-02-10 15:32:55 -0800

    lkn

--- Assets/Scripts/NewMonoBehaviourScript.cs
+++ Assets/Scripts/NewMonoBehaviourScript.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 
-public class Github : MonoBehaviour
+public class NewMonoBehaviourScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hello!");
+        
     }
 
     // Update is called once per frame
